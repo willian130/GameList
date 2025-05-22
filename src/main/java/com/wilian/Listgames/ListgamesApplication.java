b6bfcd2a59a912e7fed5815d80746d534db56e67
willian Batista <willianbatista791@gmail.com>
2025-05-22 14:05:34 -0300

    o principio

---
+++ src/main/java/com/wilian/Listgames/ListgamesApplication.java
@@ -0,0 +1,13 @@
+package com.wilian.Listgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilian/Listgames/ListgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilian.Listgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
